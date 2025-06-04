4e1b51b967a25aa3b1ddee82201980d180acd2bc
Juan Vargas <114629442+Juvardz@users.noreply.github.com>
2025-06-04 09:15:27 -0600

    Arreglo Materias
    Arreglo para que materias funcione con la nueva estructura de la tabla en la BD

--- Infrastructure/DTOs/Mantenimientos/MateriaDto.cs
+++ Infrastructure/DTOs/Mantenimientos/MateriaDto.cs
@@ -11,5 +11,7 @@ namespace Infrastructure.DTOs.Mantenimientos
         public int Id_Materia { get; set; }
         public string Nombre { get; set; }
         public string Tipo { get; set; }
+        public string Color { get; set; }
+        public string Estado { get; set; }
     }
 }
